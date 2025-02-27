32c506b63ba53fb499e5214fe5f322533d3d455a
unknown <danilic206@gmail.com>
2025-02-27 15:10:47 +0300

    Попытка перенести на новый JDK

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,8 @@ module com.cgvsu {
 
     opens com.cgvsu to javafx.fxml;
     exports com.cgvsu;
+    exports com.cgvsu.objwriter;
+    opens com.cgvsu.objwriter to javafx.fxml;
+    exports com.cgvsu.model;
+    opens com.cgvsu.model to javafx.fxml;
 }
